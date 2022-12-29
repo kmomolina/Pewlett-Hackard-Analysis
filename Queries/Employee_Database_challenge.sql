9a6db4c15f2af52d2b6c26f1a261ed8322a5dff0
kmomolina <kmomolina@gmail.com>
2022-12-29 15:50:36 -0700

    deliv 2 complete

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -28,7 +28,28 @@ ORDER BY emp_no, title DESC;
 -- Get the count of retiting employees by title in descending order
 SELECT COUNT(emp_no),
 title
---INTO retiring_titles
+INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
+
+
+--DELIVERABLE 2: The Employees Eligible for the Mentorship Program
+
+-- create a table to find eligible employees by current employment and birthdate
+SELECT DISTINCT ON (em.emp_no) em.emp_no,
+    em.first_name,
+    em.last_name,
+    em.birth_date,
+    de.from_date,
+    de.to_date,
+    t.title
+INTO mentorship_eligibility
+FROM employees as em
+INNER JOIN dept_emp as de
+    ON em.emp_no = de.emp_no
+INNER JOIN titles as t
+    ON em.emp_no = t.emp_no
+WHERE (de.to_date = '9999-01-01') AND (em.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY em.emp_no;
+
