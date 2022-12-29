bd3dbdae0b2963c8bb5a48894a4e5c1e2df91dea
kmomolina <kmomolina@gmail.com>
2022-12-29 15:00:55 -0700

    deliv 1 complete

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,34 @@
+-- Create new table for retiring employees
+SELECT em.emp_no,
+    em.first_name,
+    em.last_name,
+    t.title,
+    t.from_date,
+    t.to_date
+INTO retirement_titles
+FROM employees as em
+    INNER JOIN titles as t
+    ON (em.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+;
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) 
+emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retirement_titles
+WHERE (to_date = '9999-01-01')
+ORDER BY emp_no, title DESC;
+
+-- Get the count of retiting employees by title in descending order
+SELECT COUNT(emp_no),
+title
+--INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
